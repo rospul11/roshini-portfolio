15d965b4baee98b2458ca2f07faf4a1592b8c055
rpulle11 <95676247+rpulle11@users.noreply.github.com>
2024-08-22 12:28:10 -0700

    add: workflow

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/navbar';
+import Navbar from './components/Navbar';
 import Home from './pages/home';
 import About from './pages/About';
 import Portfolio from './pages/Portfolio';
